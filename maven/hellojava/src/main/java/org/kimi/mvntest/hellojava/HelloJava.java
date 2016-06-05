bfa5fbd283ee70dc1aedbcedabca67f6714e6aca
kimi <sunx_2003@163.com>
2016-06-05 21:40:17 +0800

    Add a maven project for hello-world.
    Signed-off-by: kimi <sunx_2003@163.com>

---
+++ maven/hellojava/src/main/java/org/kimi/mvntest/hellojava/HelloJava.java
@@ -0,0 +1,10 @@
+package org.kimi.mvntest.hellojava;
+
+/**
+ * Created by kimi on 4/24/16.
+ */
+public class HelloJava {
+    public static void main(String[] args){
+        System.out.println("HelloJava!");
+    }
+}
---
+++ maven/scalaDemo/src/main/scala/org/kimi/scalaDemo/Hello.scala
@@ -0,0 +1,11 @@
+package org.kimi.scalaDemo
+
+/**
+  * Created by kimi on 4/24/16.
+  */
+object Hello {
+  def main(args:Array[String])
+  {
+    println("Hello world!")
+  }
+}
