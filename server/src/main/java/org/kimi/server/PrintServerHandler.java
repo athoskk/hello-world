6c99a9b61b4968e598b53ff18df7fc38ec65eb69
kimi <sunx_2003@163.com>
2016-06-07 00:17:27 +0800

    add the new moudle server
    Signed-off-by: kimi <sunx_2003@163.com>

---
+++ server/src/main/java/org/kimi/server/PrintServerHandler.java
@@ -0,0 +1,30 @@
+package org.kimi.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Created by kimi on 6/7/16.
+ */
+public class PrintServerHandler extends ChannelHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
