d4c9b20bfc0973249ad82658a3357ce96ad85083
kimi <sunx_2003@163.com>
2016-07-03 22:47:15 +0800

    Add the test code for client and server for netty.
    Signed-off-by: kimi <sunx_2003@163.com>

---
+++ client/src/main/java/org/kimi/client/Client.java
@@ -0,0 +1,49 @@
+package org.kimi.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by kimi on 6/19/16.
+ */
+public class Client {
+
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+    static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
+
+    public static void main(String[] args) throws Exception {
+
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap(); // (1)
+            b.group(workerGroup); // (2)
+            b.channel(NioSocketChannel.class); // (3)
+            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    //ch.pipeline().addLast(new TimeClientHandler());
+                    ch.pipeline().addLast(new EchoClientHandler());
+                }
+            });
+
+            // Start the client.
+            ChannelFuture f = b.connect(host, port).sync(); // (5)
+
+            // Wait until the connection is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
--- notes/notes.md
+++ notes/notes.md
@@ -1,10 +1,31 @@
 ## Linux
-#### apt-get安装软件
+
+#### 安装软件
+
 我们在使用Ubuntu安装程序时经常会遇到添加软件源的操作，最常见的是ppa软件源。ppa是Ubuntu Launchpad网站提供的一项服务，允许个人用户作为apt源供其他用户下载和更新。
 ```
 sudo add-apt-repository ppa:rvm/smplayer
 sudo apt-get update
 sudo apt-get install ppa-name
+```
+
+deb包安装
+```
+sudo dpkg -i opera-stable_38.0.2220.29_i386.deb
+```
+
+下载文件使用wget命令
+```
+wget -r -np -pk -nH -P ./download http://www.baidu.com
+```
+安装rpm包
+```
+sudo alien flash-plugin-11.2.202.621-release.i386.rpm
+```
+####网络
+询进程占用哪些端口
+```
+netstat -nlap|grep 3573
 ```
 
 ## Git
